e8216d15e5853a3059d742e650858478862b88ef
Mykola <nickkozlov90@gmail.com>
2024-03-23 12:07:15 +0200

    Update url for debug-toolbar

--- used_car_marketplace/urls.py
+++ used_car_marketplace/urls.py
@@ -24,11 +24,11 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("marketplace.urls", namespace="marketplace")),
     path("accounts/", include("django.contrib.auth.urls")),
-    path("__debug__/", include("debug_toolbar.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
 # if settings.DEBUG:
-urlpatterns += static(
-    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-)
+urlpatterns += [
+    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
+    path("__debug__/", include("debug_toolbar.urls")),
+]
