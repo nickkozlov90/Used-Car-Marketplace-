22ef71fdc0b40d4072571000b383c761820e6e47
Mykola <nickkozlov90@gmail.com>
2024-03-23 14:49:07 +0200

    Add page_not_created template

--- used_car_marketplace/urls.py
+++ used_car_marketplace/urls.py
@@ -18,17 +18,21 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("marketplace.urls", namespace="marketplace")),
     path("accounts/", include("django.contrib.auth.urls")),
+    path(
+        "page_not_created/",
+        TemplateView.as_view(template_name='marketplace/page_not_created.html'),
+        name="page_not_created"
+    )
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
 if settings.DEBUG:
     urlpatterns += [
-        static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
         path("__debug__/", include("debug_toolbar.urls")),
-    ]
+        ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
